8e89b93acedb0f4954918f193f4a98a4e8e3537a
sntkn <mail.tsurumi@gmail.com>
2018-12-28 10:46:26 +0900

    Define a default_scope for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  default_scope -> { includes(:user).order(created_at: :desc) }
+  # https://techracho.bpsinc.jp/hachi8833/2017_10_31/47302
 end
