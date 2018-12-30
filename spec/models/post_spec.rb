b41764eb75ffe67c34301d03910f6f1283e9f1d9
sntkn <mail.tsurumi@gmail.com>
2018-12-30 22:44:11 +0900

    Add specs for the Post model's validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -27,23 +27,71 @@ RSpec.describe Post, type: :model do
       expect(posts.count).to eq 1
       expect(posts[0].category.name).to eq category.name
     end
+
+    it 'by_branch scope gets posts by particular branch' do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_branch(category.branch)
+      expect(posts.count).to eq 1
+      expect(posts[0].category.branch).to eq category.branch
+    end
+
+    it 'search finds a matching post' do
+      post = create(:post, title: 'awesome title', content: 'great content ' * 5)
+      create_list(:post, 10, title: ('a'..'c' * 2).to_a.shuffle.join)
+      expect(Post.search('awesome').count).to eq 1
+      expect(Post.search('awesome')[0].id).to eq post.id
+      expect(Post.search('great').count).to eq 1
+      expect(Post.search('great')[0].id).to eq post.id
+    end
   end
 
-  it 'by_branch scope gets posts by particular branch' do
-    category = create(:category)
-    create(:post, category_id: category.id)
-    create_list(:post, 10)
-    posts = Post.by_branch(category.branch)
-    expect(posts.count).to eq 1
-    expect(posts[0].category.branch).to eq category.branch
-  end
+  context 'Validations' do
+    let(:post) { build(:post) }
 
-  it 'search finds a matching post' do
-    post = create(:post, title: 'awesome title', content: 'great content ' * 5)
-    create_list(:post, 10, title: ('a'..'c' * 2).to_a.shuffle.join)
-    expect(Post.search('awesome').count).to eq 1
-    expect(Post.search('awesome')[0].id).to eq post.id
-    expect(Post.search('great').count).to eq 1
-    expect(Post.search('great')[0].id).to eq post.id
+    it 'creates successfully' do
+      expect(post).to be_valid
+    end
+
+    it 'is not valid without a category' do
+      post.category_id = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid without a title' do
+      post.title = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid without a user_id' do
+      post.user_id = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a title, shorter than 5 characters' do
+      post.title = 'a' * 4
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a title, longer than 255 characters' do
+      post.title = 'a' * 256
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid without a content' do
+      post.content = nil
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a content, shorter than 20 characters' do
+      post.content = 'a' * 19
+      expect(post).not_to be_valid
+    end
+
+    it 'is not valid with a content, longer than 1000 characters' do
+      post.content = 'a' * 1001
+      expect(post).not_to be_valid
+    end
   end
 end
